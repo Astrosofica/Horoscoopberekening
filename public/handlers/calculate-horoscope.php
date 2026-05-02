9af3a04da0ffd888332550b2423859607a437dfe
Jan <jan@example.com>
2026-05-02 22:28:47 +0200

    fix: allow digits in lastname validation for Solaar year suffix

--- public/handlers/calculate-horoscope.php
+++ public/handlers/calculate-horoscope.php
@@ -53,7 +53,7 @@ if (empty($lastname)) {
     $error = "Ongeldige voornaam";
 } elseif (!empty($infix) && !preg_match('/^[\p{L}\s\-\']+/u', $infix)) {
     $error = "Ongeldig tussenvoegsel";
-} elseif (!preg_match('/^[\p{L}\s\-\'\(\)]+$/u', $lastname)) {
+} elseif (!preg_match('/^[\p{L}\s\-\'\(\)\d]+$/u', $lastname)) {
     $error = "Ongeldige achternaam";
 } elseif (!preg_match('/^[\p{L}\s\-\.,\']+$/u', $location)) {
     $error = "Ongeldige locatie";
