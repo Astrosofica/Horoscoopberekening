89cae8d3025d4310b8d82ef0f8aecfc8d3752068
Jan <jan@example.com>
2026-05-24 16:33:03 +0200

    Bump version to 3.1.0 Beta (2026-05-24)

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
-define('BUILD_VERSION', '3.0.9 Beta');
-define('BUILD_DATE', '2026-05-03');
+define('BUILD_VERSION', '3.1.0 Beta');
+define('BUILD_DATE', '2026-05-24');
 
 define('APP_NAME', 'Horoscoopberekening');
 define('APP_AUTHOR', 'Jan van der Velde');
